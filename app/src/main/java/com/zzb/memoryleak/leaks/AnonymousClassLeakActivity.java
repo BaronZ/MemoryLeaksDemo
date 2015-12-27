7a80f6f9279f00a56e744d3e0ddd1865148f1f08
BaronZ <realbo89@outlook.com>
2015-12-27 21:53:41 +0800

    注释

---
+++ app/src/main/java/com/zzb/memoryleak/leaks/AnonymousClassLeakActivity.java
@@ -0,0 +1,57 @@
+package com.zzb.memoryleak.leaks;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.zzb.memoryleak.R;
+
+/**
+ * 匿名内部类泄露，如果没有释放，退出该activity,点击gc内存不会下降，点击realse之后，内存下降
+ * created at 2015/12/27 21:52
+ */
+public class AnonymousClassLeakActivity extends AppCompatActivity implements SingletonAnonymousLeak.Listener {
+    private SingletonAnonymousLeak.Listener mListener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_anonymous_class_leak);
+        ImageView iv = (ImageView) findViewById(R.id.iv);
+        iv.setImageResource(R.drawable.large1);
+        findViewById(R.id.btn_release).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SingletonAnonymousLeak.getInstance().release();
+            }
+        });
+        leak0();
+    }
+
+    private void leak0() {
+        SingletonAnonymousLeak.getInstance().addListener(new SingletonAnonymousLeak.Listener() {
+            @Override
+            public void doSomething() {
+            }
+        });
+    }
+
+    private void leak1() {
+        SingletonAnonymousLeak.getInstance().setListener(this);
+    }
+
+   
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        // SingletonAnonymousLeak.getInstance().release();
+    }
+
+
+    @Override
+    public void doSomething() {
+
+    }
+}
