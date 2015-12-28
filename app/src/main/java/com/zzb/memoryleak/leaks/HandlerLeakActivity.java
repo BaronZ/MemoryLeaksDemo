e567d32e984c779510fbc61e65f5e5a4abc12e3f
BaronZ <realbo89@outlook.com>
2015-12-28 23:57:21 +0800

    HandlerLeakActivity

--- app/src/main/java/com/zzb/memoryleak/leaks/HandlerLeakActivity.java
+++ app/src/main/java/com/zzb/memoryleak/leaks/HandlerLeakActivity.java
@@ -8,16 +8,18 @@ import android.widget.TextView;
 
 import com.zzb.memoryleak.R;
 
+import java.lang.ref.WeakReference;
+
 public class HandlerLeakActivity extends AppCompatActivity {
     private static final String TAG = "HandlerLeakActivity";
     private InnerStaticHandler mInnerStaticHandler;
+    private String mStringField;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_handler_leak);
         Message msg = new Message();
         msg.obj = "mHandler";
-
 //        mHandler.sendMessageDelayed(msg, 5000);
         Message msg1 = new Message();
         msg1.obj = "sHandler";
@@ -27,24 +29,43 @@ public class HandlerLeakActivity extends AppCompatActivity {
 
 
     }
-    private Handler mHandler = new Handler(new Handler.Callback() {
-        @Override
-        public boolean handleMessage(Message msg) {
-            return false;
-        }
-    });
+
 
     private class InnerHandler extends Handler{
+        private TextView tv;
+        public InnerHandler(TextView tv){
+            this.tv = tv;
+        }
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
 
+        }
     }
     private class InnerStaticHandler extends Handler{
         private TextView tv;//what about not TextView but some fields without ref to context
-        public InnerStaticHandler(TextView tv, Callback callback){
-            super(callback);
+        private String stringField;
+        public InnerStaticHandler(TextView tv){
             this.tv = tv;
+        }
+        public InnerStaticHandler(String stringField){
+            this.stringField = stringField;
+        }
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+
         }
     }
     private class InnerStaticWeakHandler extends Handler{
-        
+        private WeakReference<TextView> tvRef;
+        public InnerStaticWeakHandler(TextView tv){
+            tvRef = new WeakReference<TextView>(tv);
+        }
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+        }
     }
+
 }
--- app/src/main/java/com/zzb/memoryleak/leaks/SingletonContextLeakActivity.java
+++ app/src/main/java/com/zzb/memoryleak/leaks/SingletonContextLeakActivity.java
@@ -1,23 +1,26 @@
 package com.zzb.memoryleak.leaks;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 import com.zzb.memoryleak.R;
 
 public class SingletonContextLeakActivity extends AppCompatActivity {
-
+    private Context mContext;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_singleton_leak);
+        mContext = this;
         leak0();
     }
+    //引用了Activity，导致内存泄露
     private void leak0(){
-        SingletonContextLeak.getInstance(this);
+        SingletonContextLeak.getInstance(mContext);
     }
     //解决方法，用ApplicationContext代替Activity
     private void noLeak(){
-        SingletonContextLeak.getInstance(this.getApplicationContext());
+        SingletonContextLeak.getInstance(mContext.getApplicationContext());
     }
 }
